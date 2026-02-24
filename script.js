8454b3bcf1d154a8fa753999c9a159399b49ff1f
José Martínez <jgermanmm@gmail.com>
2026-02-24 11:13:34 -0300

    Refactor index.html for consistent indentation; add ejemplo_js.html and script.js for JavaScript example

---
+++ script.js
@@ -0,0 +1,23 @@
+  
+  
+console.log("Hola mundo")
+
+
+// Siempre accedemos desde el document
+
+document.getElementById("demo").innerHTML = "Modificando el título" // selección por id
+
+console.log(document.getElementById("demo"))
+
+console.log(document.getElementsByTagName("main"))
+
+console.log(document.getElementsByClassName("parrafo"))
+
+document.getElementsByClassName("parrafo")[2].innerHTML = "Modificando el párrafo"
+
+
+document.getElementById("principal").style.color = "red"
+document.getElementsByClassName("parrafo")[0].style.color = "blue"
+
+
+document.getElementById("principal").style.fontSize = "50px"
